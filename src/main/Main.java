a9005334e049ffbcc21daa2b46a5362c01a13486
Andrei Racautanu <andrei.racautanu@cgm.com>
2017-08-24 15:17:27 +0300

    Updated code

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,5 +3,8 @@ package main;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello GitHub!");
+		
+		String message = "This is my first uploaded project!";
+		System.out.println(message);
 	}
 }
